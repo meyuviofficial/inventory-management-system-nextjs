6dd48d0131350c6a76cdfd6f86591da997f62dfd
Yuvi <Yuvaraj.Selvarajan@shell.com>
2024-03-27 12:33:05 +0530

    enhanced the table component

--- src/app/Users/page.tsx
+++ src/app/Users/page.tsx
@@ -1,9 +1,51 @@
 import TableComponent from "@/components/TableComponent";
+const rows = [
+  {
+    key: "1",
+    name: "Tony Reichert",
+    role: "CEO",
+    status: "Active",
+  },
+  {
+    key: "2",
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    status: "Paused",
+  },
+  {
+    key: "3",
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    status: "Active",
+  },
+  {
+    key: "4",
+    name: "William Howard",
+    role: "Community Manager",
+    status: "Vacation",
+  },
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "role",
+    label: "ROLE",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+];
+
 export default function Users() {
   return (
     <>
       <div className="mx-8 mt-12 min-h-screen">
-        <TableComponent />
+        <TableComponent columns={columns} rows={rows} />
       </div>
     </>
   );
---
+++ src/components/TableComponent.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+
+export default function TableComponent({
+  rows,
+  columns,
+}: {
+  rows: { key: string; name: string; role: string; status: string }[];
+  columns: { key: string; label: string }[];
+}) {
+  return (
+    <Table aria-label="Example table with dynamic content">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => (
+              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
